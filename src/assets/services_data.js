bc68019505f777d33563a5b92531d43cb2f2cf59
shamim <shamim.sharifi123@gmail.com>
2024-08-25 16:47:00 -0600

    services are made

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,40 @@
 const Services_Data = [
-    {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-]
- 
+  {
+    s_no: "01",
+    s_name: "Database Design",
+    s_desc:
+      "Designing robust database solutions using SQL and Python to enhance data efficiency and integrity.",
+  },
+  {
+    s_no: "02",
+    s_name: "Web Development",
+    s_desc:
+      "Building responsive and dynamic websites using JavaScript, React, and Flask, tailored to meet specific client needs.",
+  },
+  {
+    s_no: "03",
+    s_name: "API Development",
+    s_desc:
+      "Creating and integrating RESTful APIs to facilitate seamless communication between web services and improve application functionality.",
+  },
+  {
+    s_no: "04",
+    s_name: "Software Troubleshooting",
+    s_desc:
+      "Diagnosing and resolving software issues, optimizing system performance, and enhancing user satisfaction.",
+  },
+  {
+    s_no: "05",
+    s_name: "Customer Support Systems",
+    s_desc:
+      "Developing customer support software solutions that enhance engagement and streamline service processes.",
+  },
+  {
+    s_no: "06",
+    s_name: "Technical Training",
+    s_desc:
+      "Providing technical training sessions on modern development practices and technologies to teams and individuals.",
+  },
+];
+
 export default Services_Data;
